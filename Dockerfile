645a6c47b5f33c3a33d911d99f448c5ad4fddcf1
juansgobba <juanpablosgobba@hotmail.com>
2025-11-19 02:17:31 -0300

    fix: :bug: fix deploy

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM node:22-alpine AS builder
 WORKDIR /app
 
 COPY package*.json ./
-RUN npm install --omit=dev
+RUN npm install
 
 COPY . .
 RUN npm run build
@@ -15,12 +15,15 @@ FROM node:22-alpine
 WORKDIR /app
 
 COPY package*.json ./
-RUN npm install --omit=dev
+RUN npm install
 
+# Copiar archivos necesarios desde el builder
 COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/node_modules ./node_modules
-COPY .env.example ./.env.example # Copiar el .env.example para referencia
-COPY ormconfig.ts ./ormconfig.ts # Necesario para TypeORM CLI si se ejecuta dentro del contenedor
+
+# Copiar archivos de configuración
+COPY .env.example ./.env.example
+COPY ormconfig.ts ./ormconfig.ts
 
 # Crear el directorio de entrada para el archivo de clientes
 RUN mkdir -p challenge/input
--- src/file/infrastructure/repositories/logger/WinstonLoggerRepository.ts
+++ src/file/infrastructure/repositories/logger/WinstonLoggerRepository.ts
@@ -14,14 +14,6 @@ const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level.toUpperCase()}]: ${message}`;
 });
 
-// Filtro para mostrar SOLO mensajes de nivel 'info' en la consola
-const onlyInfo = format((info) => {
-  if (info.level === 'info') {
-    return info;
-  }
-  return false;
-});
-
 @Injectable()
 export class WinstonLoggerRepository implements ILogger {
   private readonly logger: WinstonLoggerType;
@@ -32,12 +24,8 @@ export class WinstonLoggerRepository implements ILogger {
       format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })),
       transports: [
         new transports.Console({
-          level: 'info', // Nivel mínimo para consola (el filtro se encarga de lo demás)
-          format: combine(
-            onlyInfo(), // Aplicar el filtro para mostrar solo info
-            colorize(),
-            logFormat, // Aplicar el formato final aquí
-          ),
+          level: 'info', // Mostrar info, warn y error
+          format: combine(colorize(), logFormat),
         }),
         new transports.File({
           filename: path.join(process.cwd(), 'errors.log'),
